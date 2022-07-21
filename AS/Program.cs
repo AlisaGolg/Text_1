d1430750f61cc16a22e239484bfd8b81790d1069
Anna <afrodita-kts@mail.ru>
2022-07-21 22:42:06 +0300

    AS приветствие

--- AS/Program.cs
+++ AS/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести приветствие
+Console.WriteLine("Введите ваше имя");
+String username=Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username); 
