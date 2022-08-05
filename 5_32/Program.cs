5ce714582d37d4cc7d3d5ad37dbb917a895b1c05
Anna <afrodita-kts@mail.ru>
2022-08-05 21:43:41 +0300

    урок 5 задача 32

---
+++ 5_32/Program.cs
@@ -0,0 +1,17 @@
+﻿// // task 32
+Console.WriteLine("Введите кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+for (int i = 0; i < n; i++)
+    array[i] *= (-1);
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
